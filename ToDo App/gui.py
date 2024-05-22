d3a890b9d7c06023bc648f7143d2c1413d303c89
tokarski-to <tomasz.tokarski93@gmail.com>
2024-05-22 09:13:10 +0200

    Implement Add and Edit Button, day 17

--- ToDo App/gui.py
+++ ToDo App/gui.py
@@ -1,10 +1,40 @@
 import functions as func
-import FreeSimpleGUI as fsg
+import FreeSimpleGUI as sg
 
-label = fsg.Text('Type in a to-do')
-input_box = fsg.InputText(tooltip='Enter todo')
-add_button = fsg.Button('Add')
+label = sg.Text('Type in a to-do')
+input_box = sg.InputText(tooltip='Enter todo', key='todo')
+add_button = sg.Button('Add')
+list_box = sg.Listbox(values=func.get_todos(), key='todos',
+                      enable_events=True, size=(45, 10))
+edit_button = sg.Button('Edit')
 
-window = fsg.Window('My To-Do App', layout=[[label], [input_box, add_button]])
+window = sg.Window('My To-Do App',
+                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   font=('Helvetica', 20))
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case 'Add':
+            todos = func.get_todos()
+            new_todo = values['todo'] + '\n'
+            todos.append(new_todo)
+            func.write_todos(todos)
+            window['todos'].update(values=todos)
+        case 'Edit':
+            todos = func.get_todos()
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo'] + '\n'
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            func.write_todos(todos)
+            window['todos'].update(values=todos)
+        case 'todos':
+            window['todo'].update(value=values['todos'][0].strip('\n'))
+        case sg.WIN_CLOSED:
+            break
+
 window.read()
 window.close()
