4bbbd95d42c25b147373ba087457b46b60f61e9c
tokarski-to <tomasz.tokarski93@gmail.com>
2024-05-28 11:22:18 +0200

    Change layout into columns

--- ToDo App/gui.py
+++ ToDo App/gui.py
@@ -7,19 +7,28 @@ sg.theme('DarkTeal6')
 clock = sg.Text('', key='clock')
 label = sg.Text('Type in a to-do')
 input_box = sg.InputText(tooltip='Enter todo', key='todo')
-add_button = sg.Button('Add', size=10)
+add_button = sg.Button('Add', size=8)
 list_box = sg.Listbox(values=func.get_todos(), key='todos',
                       enable_events=True, size=(45, 10))
-edit_button = sg.Button('Edit')
-complete_button = sg.Button('Complete')
+edit_button = sg.Button('Edit', size=8)
+complete_button = sg.Button('Complete', size=8)
 exit_button = sg.Button('Exit')
 
+left_column_content = [[clock],
+                       [label],
+                       [input_box],
+                       [list_box],
+                       [exit_button]]
+
+right_column_content = [[add_button],
+                        [edit_button],
+                        [complete_button]]
+left_column = sg.Column(left_column_content)
+right_column = sg.Column(right_column_content)
+
+
 window = sg.Window('My To-Do App',
-                   layout=[[clock],
-                           [label],
-                           [input_box, add_button],
-                           [list_box, edit_button, complete_button],
-                           [exit_button]],
+                   layout=[[left_column, right_column]],
                    font=('Helvetica', 15))
 
 while True:
@@ -27,16 +36,17 @@ while True:
     if event == sg.WIN_CLOSED:
         break
     window['clock'].update(value=time.strftime('%b %d, %Y %H:%M:%S'))
-    print(event)
-    print(values)
     match event:
         case 'Add':
-            todos = func.get_todos()
-            new_todo = values['todo'] + '\n'
-            todos.append(new_todo)
-            func.write_todos(todos)
-            window['todos'].update(values=todos)
-            window['todo'].update(value='')
+            if values['todo'] == '':
+                sg.popup('The text is empty. Please write a todo.', font=("Helvetica", 15))
+            else:
+                todos = func.get_todos()
+                new_todo = values['todo'] + '\n'
+                todos.append(new_todo)
+                func.write_todos(todos)
+                window['todos'].update(values=todos)
+                window['todo'].update(value='')
         case 'Edit':
             try:
                 todos = func.get_todos()
@@ -47,7 +57,7 @@ while True:
                 func.write_todos(todos)
                 window['todos'].update(values=todos)
             except IndexError:
-                sg.popup('Please select an item first.', font=("Helvetica", 20))
+                sg.popup('Please select an item first.', font=("Helvetica", 15))
         case 'Complete':
             try:
                 todo_to_complete = values['todos'][0]
@@ -57,7 +67,7 @@ while True:
                 window['todos'].update(values=todos)
                 window['todo'].update(value='')
             except IndexError:
-                sg.popup('Please select an item first.', font=("Helvetica", 20))
+                sg.popup('Please select an item first.', font=("Helvetica", 15))
         case 'Exit':
             break
         case 'todos':
