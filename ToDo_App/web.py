95e5c0494f5bec175ab15aa810ac2864c66e411b
tokarski-to <Tomasz.Tokarski@esk-sic.de>
2024-05-31 16:41:38 +0200

    Implement Add and Complete in WebApp

--- ToDo_App/web.py
+++ ToDo_App/web.py
@@ -3,22 +3,27 @@ import functions as func
 
 todos = func.get_todos()
 def add_todo():
-    todo = st.session_state['new_todo'] + '\n'
-    todos.append(todo)
-    func.write_todos(todos)
+    if st.session_state['new_todo'] != '':
+        todo = st.session_state['new_todo'] + '\n'
+        todos.append(todo)
+        func.write_todos(todos)
 
 
 st.title('My ToDo App')
 st.subheader('This is my todo app')
 st.write('This app is to increase your productivity')
 
-for todo in todos:
-    st.checkbox(todo)
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        func.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
+    st.session_state['new_todo'] = ''
 
 
 st.text_input(label='Enter a todo', label_visibility='hidden', placeholder='Add a new todo...',
               on_change=add_todo, key='new_todo')
-
-print('Hello')
-
 st.session_state
